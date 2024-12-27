8da07e2282c0ad867bd1ca6eed3d7df65a18fa24
berkayyyilmaz <yilmaz.berkayyagiz@gmail.com>
2024-12-27 23:21:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <ToastContainer />
     </>
   )
 }
