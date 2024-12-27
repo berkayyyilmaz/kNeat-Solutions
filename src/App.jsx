47b379ca2b3b87565968243cf1065e504cf2a13b
berkayyyilmaz <yilmaz.berkayyagiz@gmail.com>
2024-12-27 23:45:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
+import '../node_modules/react-toastify/dist/ReactToastify.css'
 import { ToastContainer, toast } from 'react-toastify'; 
 function App() {
   const handleClick = () => {
